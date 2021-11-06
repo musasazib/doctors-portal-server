4b44613d513806a05bdaad5a855dd29e0da50765
musasazib <musasazib@gmail.com>
2021-11-07 00:48:58 +0600

    Added Post API

--- index.js
+++ index.js
@@ -3,13 +3,41 @@ const cors = require('cors');
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
 
-const app = express();
-app.use(cors());
-
 const port = process.env.PORT || 5000;
 
+const app = express();
+app.use(cors());
+app.use(express.json());   // Most used
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iyv3j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+    try {
+        await client.connect();
+
+        const database = client.db('doctorsPortal');
+        const appointmentCollection = database.collection('appointments');
+
+        app.post('/appointments', async (req, res) => {
+            const appointment = req.body;
+            // console.log(appointment);
+            // res.json({ message: 'hello' })
+            const result = await appointmentCollection.insertOne(appointment);
+            console.log(result);
+            res.json(result);
+
+        })
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello Doctors Portal')
